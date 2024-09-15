f56a039571a38a5d62efc6b59d3d005710a499e9
FbcGa <fabricio.alipazaga.pe@gmail.com>
2024-09-15 16:34:25 -0500

    backend models

--- src/api/models.py
+++ src/api/models.py
@@ -5,8 +5,9 @@ db = SQLAlchemy()
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    password = db.Column(db.String(180), unique=False, nullable=False)
+
+    lists = db.relationship('List', backref = 'user' )
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -15,5 +16,37 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "lists": [list.serialize() for list in self.lists]
         }
+#relación de uno a muchos User con List
+class List(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), unique=False , nullable=False)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    tasks = db.relationship('Task', backref='list')
+
+    def __repr__(self):
+        return f'<List {self.id}>'
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "tasks": [task.serialize() for task in self.tasks]
+        }
+#relación de uno a muchos List con Task    
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(100), unique=False, nullable=False)
+    list_id = db.Column(db.Integer, db.ForeignKey('list.id', ondelete='CASCADE'))
+    
+    def __repr__(self):
+        return f'<Task {self.text}>'
+    
+    def serialize(self):
+        return {
+            "list_id": self.list_id,
+            "id": self.id,
+            "text": self.text
+        }   
--- src/front/js/pages/home.jsx
+++ src/front/js/pages/home.jsx
@@ -1,37 +1,53 @@
-import React, { useContext, useState } from "react";
-import { Context } from "../store/appContext";
-import "../../styles/home.css";
+import React, { useRef, useState, useId } from "react";
 
 export const Home = () => {
-  const { store, actions } = useContext(Context);
-  const [card, setCard] = useState([]);
+  const [posts, setPosts] = useState([]);
+  const [postState, setPostState] = useState(false);
+  const inputRef = useRef();
+  const postID = useId();
 
-  const addCard = () => {
-    return (
-      <div className="form-floating">
-        <textarea
-          className="form-control"
-          placeholder="Leave a comment here"
-          id="floatingTextarea"
-        ></textarea>
-        <label htmlFor="floatingTextarea">Comments</label>
-      </div>
-    );
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setPosts([...posts, inputRef.current.value]);
+    inputRef.current.value = "";
   };
+
   return (
-    <div className="bg-secondary mt-5" style={{ width: "200px" }}>
-      <div className="d-flex">
-        <h5>To Do</h5>
+    <div className="bg-secondary mt-5" style={{ width: "300px" }}>
+      <div className="d-flex justify-content-between align-items-center">
+        <h5 className="m-0 mx-2 p-0">To Do</h5>
         <button className="btn btn-primary">
           <i className="fa-solid fa-ellipsis"></i>
         </button>
       </div>
-      <div>
-        <button className="btn btn-primary" onClick={addCard}>
-          <i className="fa-solid fa-plus"></i>
-        </button>
-        <span>Add a card</span>
-      </div>
+
+      {postState === true ? (
+        <form className="mx-1" onSubmit={handleSubmit}>
+          <textarea
+            className="form-control"
+            id="exampleFormControlTextarea1"
+            rows="2"
+            ref={inputRef}
+          ></textarea>
+          <button type="submit">Save</button>
+        </form>
+      ) : null}
+
+      {posts.length > 0 ? (
+        posts.map((post, index) => (
+          <h1 key={`${postID}-${index}`}>{post}</h1> // Generar una key única
+        ))
+      ) : (
+        <div>
+          <button
+            className="btn btn-primary"
+            onClick={() => setPostState(true)}
+          >
+            <i className="fa-solid fa-plus"></i>
+            <span>Add a card</span>
+          </button>
+        </div>
+      )}
     </div>
   );
 };
